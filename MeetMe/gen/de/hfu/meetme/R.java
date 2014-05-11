66ba79796ad2383b31138353d00b326c21022626
DaGangstaYoda <dominik.jung@hs-furtwangen.de>
2014-05-11 18:25:43 +0200

    Preferences User Profile Fragment implemented
    Added the Preferences Fragment to the main_activity.

--- MeetMe/gen/de/hfu/meetme/R.java
+++ MeetMe/gen/de/hfu/meetme/R.java
@@ -8,6 +8,12 @@
 package de.hfu.meetme;
 
 public final class R {
+    public static final class array {
+        public static final int pref_example_list_titles=0x7f070000;
+        public static final int pref_example_list_values=0x7f070001;
+        public static final int pref_sync_frequency_titles=0x7f070002;
+        public static final int pref_sync_frequency_values=0x7f070003;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -25,16 +31,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090002;
-        public static final int container=0x7f090000;
-        public static final int textView1=0x7f090001;
+        public static final int action_settings=0x7f0a0003;
+        public static final int container=0x7f0a0000;
+        public static final int preference_fragment=0x7f0a0001;
+        public static final int textView1=0x7f0a0002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
         public static final int action_settings=0x7f060002;
@@ -42,18 +49,45 @@ public final class R {
          */
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060001;
-        public static final int pref_summary_date_of_birth=0x7f06000a;
-        public static final int pref_summary_description=0x7f06000c;
-        public static final int pref_summary_firstname=0x7f060006;
-        public static final int pref_summary_lastname=0x7f060008;
-        public static final int pref_summary_username=0x7f060004;
-        public static final int pref_title_date_of_birth=0x7f060009;
-        public static final int pref_title_description=0x7f06000b;
-        public static final int pref_title_firstname=0x7f060005;
-        public static final int pref_title_lastname=0x7f060007;
         /**  Preferences 
          */
-        public static final int pref_title_username=0x7f060003;
+        public static final int pref_category_name=0x7f060003;
+        public static final int pref_default_display_name=0x7f060017;
+        public static final int pref_description_social_recommendations=0x7f060015;
+        /**  Example settings for Data & Sync 
+         */
+        public static final int pref_header_data_sync=0x7f060019;
+        /**  Strings related to Settings 
+ Example General settings 
+         */
+        public static final int pref_header_general=0x7f060013;
+        /**  Example settings for Notifications 
+         */
+        public static final int pref_header_notifications=0x7f06001c;
+        public static final int pref_ringtone_silent=0x7f06001f;
+        public static final int pref_summary_date_of_birth=0x7f06000b;
+        public static final int pref_summary_description=0x7f060011;
+        public static final int pref_summary_firstname=0x7f060007;
+        public static final int pref_summary_gender=0x7f06000d;
+        public static final int pref_summary_lastname=0x7f060009;
+        public static final int pref_summary_username=0x7f060005;
+        public static final int pref_switchTextOff_gender=0x7f06000f;
+        public static final int pref_switchTextOn_gender=0x7f06000e;
+        public static final int pref_title_add_friends_to_messages=0x7f060018;
+        public static final int pref_title_date_of_birth=0x7f06000a;
+        public static final int pref_title_description=0x7f060010;
+        public static final int pref_title_display_name=0x7f060016;
+        public static final int pref_title_firstname=0x7f060006;
+        public static final int pref_title_gender=0x7f06000c;
+        public static final int pref_title_lastname=0x7f060008;
+        public static final int pref_title_new_message_notifications=0x7f06001d;
+        public static final int pref_title_ringtone=0x7f06001e;
+        public static final int pref_title_social_recommendations=0x7f060014;
+        public static final int pref_title_sync_frequency=0x7f06001a;
+        public static final int pref_title_system_sync_settings=0x7f06001b;
+        public static final int pref_title_username=0x7f060004;
+        public static final int pref_title_vibrate=0x7f060020;
+        public static final int title_activity_settings=0x7f060012;
     }
     public static final class style {
         /** 
@@ -77,13 +111,17 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class xml {
-        public static final int preferences=0x7f040000;
+        public static final int pref_data_sync=0x7f040000;
+        public static final int pref_general=0x7f040001;
+        public static final int pref_headers=0x7f040002;
+        public static final int pref_notification=0x7f040003;
+        public static final int preferences=0x7f040004;
     }
 }
---
+++ MeetMe/src/de/hfu/meetme/SettingsActivity.java
@@ -0,0 +1,14 @@
+package de.hfu.meetme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	
+}
