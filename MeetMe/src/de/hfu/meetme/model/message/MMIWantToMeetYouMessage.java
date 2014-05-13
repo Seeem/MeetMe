6cc2766be2e9fd554d5e5820d6a6f72441974dee
Seeem <simeonsembach@simeons-air.fritz.box>
2014-05-13 15:15:47 +0200

    New Messages

---
+++ MeetMe/src/de/hfu/meetme/model/message/MMIWantToMeetYouMessage.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package de.hfu.meetme.model.message;
+
+
+/**
+ * @author Simeon Sembach
+ *
+ */
+public class MMIWantToMeetYouMessage extends MMMessage
+{
+
+	// Class-Members:
+	
+	/** */
+	private static final long serialVersionUID = -6729471138678940987L;
+
+	// Constructor:
+
+	/** */
+	public MMIWantToMeetYouMessage(String aId)
+	{
+		setId(aId);
+	}
+
+	// Accessors (Instance):
+
+	/** */
+	public void setId(String aId)
+	{
+		super.setMessage(aId);
+	}
+	
+	/** */
+	public String getId()
+	{
+		return (String) super.getMessage();
+	}
+
+}
